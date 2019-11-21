7a86bb0d87cc057e652ea34c62f390f6ffb2d426
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-21 14:00:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
    resp = Rack::Response.new
    req = Rack::Request.new(env)
 
-   if req.path=="/items/"
+   if req.path=="/items/#{item_name}"
      item_name = req.path.split("/items/").last
      binding.pry
      if @@item.include?(item_name)
