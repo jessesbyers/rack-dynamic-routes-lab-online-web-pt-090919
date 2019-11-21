68a4b76db2b49a11c5e1fb8eac0bb598730a739b
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-21 13:54:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
    if req.path=="/items/"
      item_name = req.path.split("/items/").last
-     if @@item.include?(item_name)
+     if Item.all.include?(item_name)
        resp.write "#{item_name.price}"
      else
        resp.write "Item not found"
