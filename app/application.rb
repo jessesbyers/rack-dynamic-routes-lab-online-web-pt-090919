6e3e80ad226d7d0c074254ffa3578b4a3b6e6f47
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-21 13:38:39 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+ def call(env)
+   resp = Rack::Response.new
+   req = Rack::Request.new(env)
+
+  #  item_name = req.params["q"]
+
+   if req.path=="/items/#{item_name}"
+     resp.write "You requested the songs"
+   else
+     resp.write "Route not found"
+     resp.status = 404
+   end
+
+   resp.finish
+ end
+end
