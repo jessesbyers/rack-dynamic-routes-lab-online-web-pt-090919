4e44d260aba5ca8538898b1f0389aa3b6ea4f614
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-21 14:26:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,10 +17,10 @@ class Application
        resp.status = 400
      end
 
-  #  else
-  #    resp.write "Route not found"
-  #    resp.status = 404
-  #  end
+   else
+     resp.write "Route not found"
+     resp.status = 404
+   end
 
    resp.finish
  end
