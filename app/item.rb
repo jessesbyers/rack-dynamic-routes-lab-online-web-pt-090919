5386121412ee975ff22e098463962f2c49653e60
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-21 14:33:29 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -1,14 +1,8 @@
 class Item
   attr_accessor :name, :price
-  # @@item = []
 
   def initialize(name,price)
     @name = name
     @price = price
-    # @@item << self
   end
-
-  # def self.all
-  #   @@item
-  # end
 end
