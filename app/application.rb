d1efd38b71f45202e9f4cd5121242d391c5ebde9
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-21 14:22:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,14 +6,15 @@ class Application
    resp = Rack::Response.new
    req = Rack::Request.new(env)
 
-   if req.path.match(/items/)
-     item_name = req.path.split("/items/").last
-     item = @@items.find{|i| i.name == item_name}
-     resp.write "#{item.price}"
-     resp.status = 200
-   else
-     resp.write "Item not found"
-     resp.status = 400
+   if req.path=="/songs"
+     if req.path.match(/items/)
+       item_name = req.path.split("/items/").last
+       item = @@items.find{|i| i.name == item_name}
+       resp.write "#{item.price}"
+       resp.status = 200
+     else
+       resp.write "Item not found"
+       resp.status = 400
      end
 
    else
